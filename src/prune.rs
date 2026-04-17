8ae278cd379d927cd94d71f75b511eb342e5ad49
Your Name <undefined@example.com>
2026-04-17 23:18:38 +0100

    Preference list for pruning

--- src/prune.rs
+++ src/prune.rs
@@ -14,7 +14,7 @@ pub fn prune(args: &PruneArguments) -> anyhow::Result<ExitCode> {
     let dependencies: Vec<_> = crate::package::dependencies(&args.common, &metadata).collect();
     let licenses = crate::local::output_folder_licenses(&args.common.license_directory);
     let licenses = crate::identity::identified_licenses(&licenses)?;
-    let extraneous: Vec<_> = extraneous_licenses(&dependencies, &licenses);
+    let extraneous: Vec<_> = extraneous_licenses(&args.licenses, &dependencies, &licenses);
 
     reporter.info(format!(
         "removing {} extraneous license(s)",
@@ -29,10 +29,14 @@ pub fn prune(args: &PruneArguments) -> anyhow::Result<ExitCode> {
     Ok(reporter.exit_code())
 }
 
-fn extraneous_licenses(dependencies: &[Package], licenses: &[IdentifiedLicense]) -> Vec<PathBuf> {
+fn extraneous_licenses(
+    preference: &[spdx::Licensee],
+    dependencies: &[Package],
+    licenses: &[IdentifiedLicense],
+) -> Vec<PathBuf> {
     let requirements: Vec<_> = dependencies
         .iter()
-        .flat_map(|package| package_requirements(package, licenses))
+        .flat_map(|package| package_requirements(preference, package, licenses))
         .collect();
     licenses
         .iter()
@@ -41,7 +45,11 @@ fn extraneous_licenses(dependencies: &[Package], licenses: &[IdentifiedLicense])
         .collect()
 }
 
-fn package_requirements(package: &Package, licenses: &[IdentifiedLicense]) -> Vec<PathBuf> {
+fn package_requirements(
+    preference: &[spdx::Licensee],
+    package: &Package,
+    licenses: &[IdentifiedLicense],
+) -> Vec<PathBuf> {
     let package_licenses: Vec<_> = licenses
         .iter()
         .filter(|l| l.license.package == package.name)
@@ -49,7 +57,7 @@ fn package_requirements(package: &Package, licenses: &[IdentifiedLicense]) -> Ve
     package
         .spdx_license
         .as_ref()
-        .and_then(|expression| minimal_requirements(expression, &package_licenses))
+        .and_then(|expression| minimal_requirements(preference, expression, &package_licenses))
         .unwrap_or_else(|| {
             package_licenses
                 .into_iter()
@@ -59,15 +67,18 @@ fn package_requirements(package: &Package, licenses: &[IdentifiedLicense]) -> Ve
 }
 
 fn minimal_requirements(
+    preference: &[spdx::Licensee],
     expression: &spdx::Expression,
     licenses: &[&IdentifiedLicense],
 ) -> Option<Vec<PathBuf>> {
-    let mut licensees: Vec<_> = licenses
-        .iter()
-        .flat_map(|l| l.ids())
-        .map(licensee_from_id)
-        .collect();
-    licensees.sort_by_key(|l| l.as_ref().license.to_string());
+    let licensees: Vec<_> = sort_requirements(
+        preference,
+        licenses
+            .iter()
+            .flat_map(|l| l.ids())
+            .map(licensee_from_id)
+            .collect(),
+    );
     let requirements = match expression.minimized_requirements(&licensees) {
         Ok(requirements) => requirements,
         Err(_) => return None,
@@ -79,6 +90,24 @@ fn minimal_requirements(
             .map(|l| l.license.location.clone())
             .collect(),
     )
+}
+
+fn sort_requirements(
+    preference: &[spdx::Licensee],
+    mut requirements: Vec<spdx::Licensee>,
+) -> Vec<spdx::Licensee> {
+    requirements.sort_by(|item_a, item_b| {
+        let pos_a = preference.iter().position(|x| x == item_a);
+        let pos_b = preference.iter().position(|x| x == item_b);
+
+        match (pos_a, pos_b) {
+            (Some(a), Some(b)) => a.cmp(&b),
+            (Some(_), None) => std::cmp::Ordering::Less,
+            (None, Some(_)) => std::cmp::Ordering::Greater,
+            (None, None) => item_a.cmp(item_b),
+        }
+    });
+    requirements
 }
 
 fn required_license(requirements: &[LicenseReq], license: &IdentifiedLicense) -> bool {
--- todo.md
+++ todo.md
@@ -7,14 +7,14 @@
 - [ ] Add suggested fixes in the reports?
 - [ ] more commands
     - [ ] Add a "add" command for quickly adding allow/warn/deny rules?
-    - [ ] "prune" command for removing extra unneeded license
-        - [ ] priority list for desired licenses? MIT > Apache?
+
 - [ ] extra lints
     - [ ] Lint for unused allow/warn/deny flags/config
     - [ ] lint for deciding minimal set of licenses needed and warning on extra ones that aint needed
     - [ ] use root projects license expression to decide if copyleft is a problem?
     - [ ] Compare projects SPDX license (if any) with compatibility with licenses
     - [ ] Warn if no "license-file" file filled out in cargo
+- [ ] Error for "cargo run -- prune INVALID" is weirdly formatted
 
 # Done
 - [x] Don't use "tests" license to fix Pixar detection bug in identity: use a license file just for that in src
@@ -22,6 +22,8 @@
 - [x] Consistent report order (by level, then alphabetically?)
 - [x] Allow/warn/deny lints in Cargo.toml
 - [x] Warn if no "license" file filled out in cargo
+- [x] "prune" command for removing extra unneeded license
+    - [x] priority list for desired licenses e.g. MIT > Apache
 
 # Dont Do
 - [ ] Combine "missing" and "unmet-spdx" alerts together?
