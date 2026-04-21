c3a6512138ca2c8a3f8dd38937c9399050994aa0
Jacob Araiza <jacob.araiza@paconsulting.com>
2026-04-21 16:04:01 +0100

    Refactoring towards better test injection

--- src/config.rs
+++ src/config.rs
@@ -1,8 +1,10 @@
-use crate::{CommonConfig, FilterConfig};
+use crate::filter::Filter;
 use anyhow::Context;
 pub use cargo_metadata::Metadata;
+use clap::{Parser, ValueEnum};
 use serde::Deserialize;
-use std::path::Path;
+use std::path::{Path, PathBuf};
+use std::str::FromStr;
 
 pub fn crate_metadata(project_directory: &Path) -> anyhow::Result<Metadata> {
     cargo_metadata::MetadataCommand::new()
@@ -11,7 +13,7 @@ pub fn crate_metadata(project_directory: &Path) -> anyhow::Result<Metadata> {
         .context("failed to execute cargo metadata")
 }
 
-pub fn config(metadata: &Metadata) -> anyhow::Result<Config> {
+pub fn parse_metadata_config(metadata: &Metadata) -> anyhow::Result<Config> {
     let package = metadata
         .packages
         .iter()
@@ -30,4 +32,107 @@ pub struct Config {
 
     #[serde(flatten)]
     pub filter: FilterConfig,
+}
+
+#[derive(Deserialize, Parser)]
+pub struct CommonConfig {
+    #[serde(default = "default_project_directory")]
+    #[clap(short, long, default_value = "./")]
+    /// Path to the root folder of the project to find dependencies for
+    pub project_directory: PathBuf,
+    #[serde(default = "default_output_directory")]
+    #[clap(short, long, default_value = "./licenses/")]
+    /// Path to the folder to store license files
+    pub license_directory: PathBuf,
+    #[serde(default)]
+    #[clap(short, long)]
+    /// Package names to exclude from searching for license files (and their dependencies)
+    pub excluded: Vec<String>,
+    #[serde(default)]
+    #[clap(short, long, default_value_t = false)]
+    /// Include dependencies only used during build
+    pub build_dependencies: bool,
+    #[serde(default)]
+    #[clap(short = 'v', long, default_value_t = false)]
+    /// Include dependencies only used during dev (testing)
+    pub dev_dependencies: bool,
+    #[serde(default)]
+    #[clap(short, long, default_value_t = false)]
+    /// Do not print any logging to stderr
+    pub quiet: bool,
+}
+
+impl CommonConfig {
+    pub fn overwrite_with(self, other: Self) -> Self {
+        Self {
+            project_directory: if other.project_directory != default_project_directory() {
+                other.project_directory
+            } else {
+                self.project_directory
+            },
+            license_directory: if other.license_directory != default_output_directory() {
+                other.license_directory
+            } else {
+                self.license_directory
+            },
+            excluded: self.excluded.into_iter().chain(other.excluded).collect(),
+            build_dependencies: self.build_dependencies || other.build_dependencies,
+            dev_dependencies: self.dev_dependencies || other.dev_dependencies,
+            quiet: self.quiet || other.quiet,
+        }
+    }
+}
+
+#[derive(ValueEnum, Clone, Copy)]
+pub enum SearchRemote {
+    /// never search remotely for license files, only locally
+    Never,
+    /// search remotely for license files only if none are found locally
+    IfNotLocal,
+    /// always search remotely licenses, even if one or more found locally
+    Always,
+}
+
+#[derive(Deserialize, Parser)]
+pub struct FilterConfig {
+    #[serde(default, deserialize_with = "filters_from_strings")]
+    #[clap(short, long, value_name = "LINT_NAME[:SUB_FILTER]")]
+    /// Allow violations of this specific lint, reporting as info only. Sub filters always override non-sub ones.
+    pub allow: Vec<Filter>,
+    #[serde(default, deserialize_with = "filters_from_strings")]
+    #[clap(short, long, value_name = "LINT_NAME[:SUB_FILTER]")]
+    /// Warn on violations of this specific lint. Override allow if set. Sub filters always override non-sub ones.
+    pub warn: Vec<Filter>,
+    #[serde(default, deserialize_with = "filters_from_strings")]
+    #[clap(short, long, value_name = "LINT_NAME[:SUB_FILTER]")]
+    /// Deny violations of this specific lint, reporting as an error. Overrides allow or warn if set. Sub filters always override non-sub ones.
+    pub deny: Vec<Filter>,
+}
+
+impl FilterConfig {
+    pub fn overwrite_with(self, other: Self) -> Self {
+        Self {
+            allow: self.allow.into_iter().chain(other.allow).collect(),
+            warn: self.warn.into_iter().chain(other.warn).collect(),
+            deny: self.deny.into_iter().chain(other.deny).collect(),
+        }
+    }
+}
+
+fn filters_from_strings<'de, D>(deserializer: D) -> Result<Vec<Filter>, D::Error>
+where
+    D: serde::Deserializer<'de>,
+{
+    Vec::<String>::deserialize(deserializer)?
+        .into_iter()
+        .map(|s| Filter::from_str(&s).map_err(serde::de::Error::custom))
+        .collect()
+}
+
+fn default_project_directory() -> PathBuf {
+    "./".into()
+}
+
+fn default_output_directory() -> PathBuf {
+    "./licenses/".into()
 }
--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,15 @@ mod report;
 mod reporter;
 mod summary;
 
-use crate::filter::Filter;
+use crate::config::{CommonConfig, FilterConfig, SearchRemote};
 use crate::lint::Lint;
-use clap::{Parser, ValueEnum};
-use serde::Deserialize;
-use std::path::PathBuf;
+use clap::Parser;
 use std::process::ExitCode;
-use std::str::FromStr;
 
 fn main() -> anyhow::Result<ExitCode> {
     let args = Command::parse();
     let metadata = config::crate_metadata(args.project_directory())?;
-    let config = config::config(&metadata)?;
+    let config = config::parse_metadata_config(&metadata)?;
     let reporter = reporter::Reporter::new(config.common.quiet);
     match args {
         Command::Get(args) => get::get(args),
@@ -87,7 +84,7 @@ impl CheckArguments {
     fn overwrite(self, config: config::Config) -> config::Config {
         config::Config {
             common: config.common.overwrite_with(self.common),
-            ..config
+            filter: config.filter.overwrite_with(self.filter),
         }
     }
 }
@@ -99,107 +96,4 @@ struct PruneArguments {
 
     /// License names in preference order to keep. Otherwise will arbitrarily prefer alphabetical (e.g. Apache-2.0 > MIT > Unlicense).
     licenses: Vec<spdx::Licensee>,
-}
-
-#[derive(Deserialize, Parser)]
-struct CommonConfig {
-    #[serde(default = "default_project_directory")]
-    #[clap(short, long, default_value = "./")]
-    /// Path to the root folder of the project to find dependencies for
-    project_directory: PathBuf,
-    #[serde(default = "default_output_directory")]
-    #[clap(short, long, default_value = "./licenses/")]
-    /// Path to the folder to store license files
-    license_directory: PathBuf,
-    #[serde(default)]
-    #[clap(short, long)]
-    /// Package names to exclude from searching for license files (and their dependencies)
-    excluded: Vec<String>,
-    #[serde(default)]
-    #[clap(short, long, default_value_t = false)]
-    /// Include dependencies only used during build
-    build_dependencies: bool,
-    #[serde(default)]
-    #[clap(short = 'v', long, default_value_t = false)]
-    /// Include dependencies only used during dev (testing)
-    dev_dependencies: bool,
-    #[serde(default)]
-    #[clap(short, long, default_value_t = false)]
-    /// Do not print any logging to stderr
-    quiet: bool,
-}
-
-impl CommonConfig {
-    pub fn overwrite_with(self, other: Self) -> Self {
-        Self {
-            project_directory: if other.project_directory != default_project_directory() {
-                other.project_directory
-            } else {
-                self.project_directory
-            },
-            license_directory: if other.license_directory != default_output_directory() {
-                other.license_directory
-            } else {
-                self.license_directory
-            },
-            excluded: self.excluded.into_iter().chain(other.excluded).collect(),
-            build_dependencies: self.build_dependencies || other.build_dependencies,
-            dev_dependencies: self.dev_dependencies || other.dev_dependencies,
-            quiet: self.quiet || other.quiet,
-        }
-    }
-}
-
-#[derive(ValueEnum, Clone, Copy)]
-enum SearchRemote {
-    /// never search remotely for license files, only locally
-    Never,
-    /// search remotely for license files only if none are found locally
-    IfNotLocal,
-    /// always search remotely licenses, even if one or more found locally
-    Always,
-}
-
-#[derive(Deserialize, Parser)]
-pub struct FilterConfig {
-    #[serde(default, deserialize_with = "filters_from_strings")]
-    #[clap(short, long, value_name = "LINT_NAME[:SUB_FILTER]")]
-    /// Allow violations of this specific lint, reporting as info only. Sub filters always override non-sub ones.
-    pub allow: Vec<Filter>,
-    #[serde(default, deserialize_with = "filters_from_strings")]
-    #[clap(short, long, value_name = "LINT_NAME[:SUB_FILTER]")]
-    /// Warn on violations of this specific lint. Override allow if set. Sub filters always override non-sub ones.
-    pub warn: Vec<Filter>,
-    #[serde(default, deserialize_with = "filters_from_strings")]
-    #[clap(short, long, value_name = "LINT_NAME[:SUB_FILTER]")]
-    /// Deny violations of this specific lint, reporting as an error. Overrides allow or warn if set. Sub filters always override non-sub ones.
-    pub deny: Vec<Filter>,
-}
-
-impl FilterConfig {
-    pub fn overwrite_with(self, other: Self) -> Self {
-        Self {
-            allow: self.allow.into_iter().chain(other.allow).collect(),
-            warn: self.warn.into_iter().chain(other.warn).collect(),
-            deny: self.deny.into_iter().chain(other.deny).collect(),
-        }
-    }
-}
-
-fn filters_from_strings<'de, D>(deserializer: D) -> Result<Vec<Filter>, D::Error>
-where
-    D: serde::Deserializer<'de>,
-{
-    Vec::<String>::deserialize(deserializer)?
-        .into_iter()
-        .map(|s| Filter::from_str(&s).map_err(serde::de::Error::custom))
-        .collect()
-}
-
-fn default_project_directory() -> PathBuf {
-    "./".into()
-}
-
-fn default_output_directory() -> PathBuf {
-    "./licenses/".into()
 }
